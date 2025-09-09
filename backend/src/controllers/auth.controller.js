79f3cdf8057a9ecff6171947c6d7e5fff3761f9f
Sarthak12903 <sarthak12903@gmail.com>
2025-09-09 17:50:37 +0530

    registeration module addded

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,10 +1,22 @@
-import pool from "../config/db";
+import pool from "../config/db.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/generateTokem.js";
+
+const options = {
+  maxAge: 1000 * 60 * 60 * 24,
+
+  httpOnly: true,
+
+  secure: true,
+
+  sameSite: "strict",
+};
 
 export const register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, address } = req.body;
 
   try {
-    if (!name || !email || !password) {
+    if (!name || !email || !password || !address) {
       return res.status(400).json({ message: "All details must be filled up" });
     }
 
@@ -15,13 +27,36 @@ export const register = async (req, res) => {
     }
 
     const existingUser = await pool.query(
-      "Select*from users where email = $1",
+      "SELECT * FROM users WHERE email = $1",
       [email]
     );
 
-    if (existingUser) {
+    if (existingUser.rows.length > 0) {
       return res.status(400).json({ message: "Email already exist" });
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPass = await bcrypt.hash(password, salt);
+
+    const insertQuery = `
+      INSERT INTO users (name, email, password,address) 
+      VALUES ($1, $2, $3,$4) 
+      RETURNING id, name, email,address, created_at, updated_at
+    `;
+
+    const newUser = await pool.query(insertQuery, [
+      name,
+      email,
+      hashPass,
+      address,
+    ]);
+    const user = newUser.rows[0];
+    const token = generateToken(user.id);
+
+    return res
+      .status(201)
+      .cookie("token", token, options)
+      .json({ message: `Account Created!!!`, user });
   } catch (error) {
     return res
       .status(500)
